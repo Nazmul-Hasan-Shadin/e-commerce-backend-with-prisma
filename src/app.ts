be74b11a03410d50e4f6ab179d66b1df91be3d07
Nazmul Hasan Shadin <nazmulhasanshadin000@gmail.com>
2025-10-02 16:07:03 +0600

    cors issue fiexd

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 app.use(
   cors({
     origin: [
-      "https://independent-mart.vercel.appg",
+      "https://independent-mart.vercel.app",
     ],
     credentials: true,
   })
